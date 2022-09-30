dad8880e3028a087780c1b44c4ed5cd9f7922818
frank yanez <frankyanez9413@gmail.com>
2022-09-29 21:34:23 -0300

    productos renderizados con carrito

--- js/app.js
+++ js/app.js
@@ -1,71 +1,118 @@
 const selectProductos = document.getElementById('selectProductos');
+const container = document.getElementById('productos');
+const cartCount = document.getElementById('cartCount');
+const totalPrice = document.getElementById('totalPrice');
+const btonComprar = document.querySelector('.button')
 const carrito = [];
 
-selectProductos.addEventListener("change", () => {
-  console.log(selectProductos.value);
-  if (selectProductos.value === "Todos") {
-    showAllProducts(stockProductos);
-  } else {
-    const filtrado = showAllProducts(
-      stockProductos.filter(
-        (element) => element.categoria == selectProductos.value
-      )
-    );
-    console.log(filtrado);
-  }
+selectProductos.addEventListener('change', () => {
+  selectProductos.value === 'Todos' ? showAllProducts(stockProductos) : showAllProducts(
+    stockProductos.filter(
+      (element) => element.categoria == selectProductos.value
+    )
+  );
 });
 
 
+showAllProducts(stockProductos);
 
-function showAllProducts() {
-  const container = document.getElementById("productos");
 
-  stockProductos.forEach((producto) => {
+
+function showAllProducts(array) {
+  
+  container.innerHTML = "";
+  
+
+  array.forEach((producto) => {
     const div = document.createElement("div");
     div.classList.add("card");
     div.innerHTML = `<div>
-  <h4>${producto.nombre}</h4>
-  <img class="img-card" src="${producto.img}" alt="">
-  <h5>${producto.categoria}</h5>
-  <h6>$${producto.precio}</h6>
-  <button class="button" id="${producto.id}" >Comprar</button>
-  </div>`;
+      <h4>${producto.nombre}</h4>
+      <img class="img-card" src="${producto.img}" alt="">
+      <h5>${producto.categoria}</h5>
+      <h6>$${producto.precio}</h6>
+      <h6>${producto.cantidad}</h6>
+      <button class="button" id="${producto.id}" >Comprar</button>
+      </div>`;
 
     container.appendChild(div);
 
-    div.querySelector('button').addEventListener('click', ()=>{
-      agregarProductosAlCarrito(producto.id)
-    })
+    div.querySelector("button").addEventListener("click", () => {
+      agregarProductosAlCarrito(producto.id);
+    });
   });
-};
+}
 
-showAllProducts();
+
+
 
 function agregarProductosAlCarrito(id) {
-  let productoEncontrado = stockProductos.find(producto => producto.id === id)
+
+  let productoEncontrado = stockProductos.find(
+    (producto) => producto.id === id);
+
+    console.log(productoEncontrado);
+
+    let productoEnCarrito = carrito.find(producto => producto.id === id)
+
+    if (productoEnCarrito) {
+      productoEnCarrito.cantidad++;
+      carritoActualizado()
+    }else {
+
+      carrito.push(productoEncontrado);
+      carritoActualizado()
+    
+  }
+  showProductsCarrito();
   
-  carrito.push(productoEncontrado)
-  
-  showProductsCarrito()
 
 }
+
+
 
 
 function showProductsCarrito() {
-  const carritoProductos = document.getElementById('carrito');
 
-  carrito.forEach((producto,id)=>{
+  
+  const carritoProductos = document.getElementById("carrito");
+
+  carritoProductos.innerHTML='';
+
+  carrito.forEach((producto, id) => {
     const div = document.createElement("div");
     div.classList.add("card");
     div.innerHTML = `<div>
-  <h4>${producto.nombre}</h4>
-  <img class="img-card" src="${producto.img}" alt="">
-  <h5>${producto.categoria}</h5>
-  <h6>$${producto.precio}</h6>
-  <button class="button">Eliminar</button>
-  </div>`;
+      <h4>${producto.nombre}</h4>
+      <img class="img-card" src="${producto.img}" alt="">
+      <h5>${producto.categoria}</h5>
+      <h6>$${producto.precio}</h6>
+      <p>Cantidad: ${producto.cantidad}</p>
+      <button class="button">Eliminar</button>
+      </div>`;
 
-  carritoProductos.appendChild(div)
-  })
+    carritoProductos.appendChild(div);
+  });
+
+
 }
+
+
+function carritoActualizado() {
+  cartCount.innerText = carrito.length;
+  totalPrice.innerText = carrito.reduce((acc,el)=>acc + el.precio,0);
+  
+};
+
+
+
+
+
+
+
+
+
+
+
+
 
