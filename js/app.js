613ffc6974143dd5fb061615e9fcb8caaf126bc0
frank yanez <frankyanez9413@gmail.com>
2022-10-10 21:59:03 -0300

    correccion

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 const selectProductos = document.getElementById("selectProductos");
 const container = document.getElementById("productos");
-const btonComprar = document.getElementsByClassName(".button");
+
 const btonDelete = document.getElementsByClassName('button-delete')
 
-const carrito = [];
+const carrito = JSON.parse(localStorage.getItem('carrito'))||[];
 
 selectProductos.addEventListener("change", () => {
   selectProductos.value === "Todos"
@@ -21,21 +21,26 @@ function showAllProducts(array) {
   container.innerHTML = "";
 
   array.forEach((producto) => {
+    const {id,nombre,precio,categoria,img,cantidad} = producto
     const div = document.createElement("div");
     div.classList.add("card");
     div.innerHTML = `<div>
-      <h4>${producto.nombre}</h4>
-      <img class="img-card" src="${producto.img}" alt="">
-      <h5>${producto.categoria}</h5>
-      <h6>$${producto.precio}</h6>
-      <h6>${producto.cantidad}</h6>
-      <button class="button" id="${producto.id}" >Comprar</button>
+      <h4>${nombre}</h4>
+      <img class="img-card" src="${img}" alt="">
+      <h5>${categoria}</h5>
+      <h6>$${precio}</h6>
+      <h6>${cantidad}</h6>
+      <button class="button buttonComprar" id="${id}" >Comprar</button>
       </div>`;
+      
 
     container.appendChild(div);
 
+    
+
     div.querySelector("button").addEventListener("click", () => {
       agregarProductosAlCarrito(producto.id);
+      comprarToasty()
     });
   });
 }
@@ -76,14 +81,15 @@ function showProductsCarrito() {
     div.querySelector(".button-delete").addEventListener("click", () => {
       deleteProducts(index);
       
+      
     });
 
     carritoProductos.appendChild(div);
-    masVendidos(producto.img)
+    masVendidos(producto)
+    
   });
   
 
-  getLocal(carritoProductos);
 }
 
 function deleteProducts(index) {
@@ -94,6 +100,8 @@ function deleteProducts(index) {
   }
   showProductsCarrito();
   carritoTotal();
+  setLocal(carrito);
+  eliminarToasty();
 }
 
 function carritoTotal() {
@@ -103,29 +111,59 @@ function carritoTotal() {
     total += producto.precio * producto.cantidad
   })
 
+  console.log(total);
 
 
-  // const t = document.getElementById('total');
-  // t.innerHTML = `<h5>${total}</h5>`
+  const t = document.getElementById('total');
+
+  t.innerHTML = `<h5>${total}</h5>`
   
-  // console.log(total);
+  console.log(total);
 }
 
-function setLocal(item) {
-  const formatJson = JSON.stringify(item);
-
-  localStorage.setItem("item", formatJson);
-}
-
-function getLocal(item) {
-  const formatJson = JSON.parse(localStorage.getItem(item));
+function setLocal(carrito) {
+  localStorage.setItem('carrito', JSON.stringify(carrito));
 }
 
 
-function masVendidos(...objeto) {
+
+
+function masVendidos(objeto) {
   const agregarImagenes = document.getElementById('carritoImagenes');
+  
+  console.log(objeto.img);
 
-  agregarImagenes.innerHTML='';
+  agregarImagenes.innerHTML = `
+  <img src='${objeto.img}'>`
+
+
   
   
+  
+};
+
+// const btonComprar = document.querySelector(".buttonComprar");
+// btonComprar.addEventListener('click',comprarToasty())
+// console.log(btonComprar);
+
+const comprarToasty = () => {
+  Toastify({
+    
+    text: "Producto Añadido",
+  style: {
+    background: "linear-gradient(to right, #00b09b, #96c93d)"
+  }}).showToast()
+};
+
+const eliminarToasty = () => {
+  Toastify({
+    
+    text: "Producto Eliminado",
+  style: {
+    background: "linear-gradient(to right, rgb(150, 2, 0), #fff)"
+  }}).showToast()
 }
+
+showProductsCarrito()
+
+
